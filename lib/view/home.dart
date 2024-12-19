db506d127557815768d0e610f66e8f49413c4ee1
Thong Thien <thongthien2004@gmail.com>
2024-12-19 22:41:21 +0700

    Profile, view order, view order details Done

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,4 @@
-import 'package:book_store/view/userInfo.dart';
+import 'package:book_store/view/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controller/book_controller.dart';
